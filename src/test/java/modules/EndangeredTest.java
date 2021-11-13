f6f3a97708a8ff24a30877cb295ee54e07dbabba
carlos kipkoech <carlos.kipkoech@student.moringaschool.com>
2021-11-13 12:02:06 +0300

    uncomment testfiles

--- src/test/java/modules/EndangeredTest.java
+++ src/test/java/modules/EndangeredTest.java
@@ -1,96 +1,96 @@
-//package modules;
-//import org.junit.*;
-//
-//import java.sql.Timestamp;
-//import java.util.Date;
-//
-//import static org.junit.Assert.*;
-//
-//public class EndangeredTest {
-//
-//    public Endangered setUpNewEndangered() {
-//        return new Endangered("rhino","ill","young");
-//    }
-//
-//    @Rule
-//    public DatabaseRule database = new DatabaseRule();
-//
-//    @Test
-//    public void Endangered_instantiatesCorrectly_true() {
-//        Endangered testEndangered = setUpNewEndangered();
-//        assertEquals(true,testEndangered instanceof Endangered);
-//    }
-//
-//    @Test
-//    public void getName_instantiatesWithName_String() {
-//        Endangered testEndangered = setUpNewEndangered();
-//        assertEquals("rhino", testEndangered.getName());
-//    }
-//
-//
-//    @Test
-//    public void getHealth_instantiatesWithHealth_String() {
-//        Endangered testEndangered = setUpNewEndangered();
-//        assertEquals("ill", testEndangered.getHealth());
-//    }
-//
-//    @Test
-//    public void getAge_instantiatesWithAge_String() {
-//        Endangered testEndangered = setUpNewEndangered();
-//        assertEquals("young", testEndangered.getAge());
-//    }
-//
-//
-//
-//    @Test
-//    public void equals_returnsTrueIfObjectsSame_true() {
-//        Endangered testEndangered = setUpNewEndangered();
-//        Endangered otherEndangered = setUpNewEndangered();
-//        assertTrue(testEndangered.equals(otherEndangered));
-//    }
-//
-//    @Test
-//    public void save_savesObjectsToDatabase_true() {
-//        Endangered testEndangered = setUpNewEndangered();
-//        testEndangered.save();
-//        assertTrue(Endangered.all().get(0).equals(testEndangered));
-//    }
-//
-//    @Test
-//    public void save_assignsIdToEndangered() {
-//        Endangered testEndangered = setUpNewEndangered();
-//        testEndangered.save();
-//        Endangered savedEndangered = Endangered.all().get(0);
-//        assertEquals(savedEndangered.getId(), testEndangered.getId());
-//    }
-//
-//    @Test
-//    public void all_returnsAllInstancesOfEndangered_true() {
-//        Endangered testEndangered = setUpNewEndangered();
-//        testEndangered.save();
-//        Endangered otherEndangered = new Endangered("","","");
-//        otherEndangered.save();
-//        assertEquals(true,Endangered.all().get(0).equals(testEndangered));
-//        assertEquals(true,Endangered.all().get(1).equals(otherEndangered));
-//    }
-//
-//    @Test
-//    public void find_returnsEndangeredWithSameId_otherEndangered() {
-//        Endangered firstEndangered =setUpNewEndangered();
-//        firstEndangered.save();
-//        Endangered secondEndangered = new Endangered("","","");
-//        secondEndangered.save();
-//        assertEquals(Endangered.find(secondEndangered.getId()), secondEndangered);
-//    }
-//
-//    @Test
-//    public void delete_deletesEndangeredAnimal_true() {
-//        Endangered testEndangered = setUpNewEndangered();
-//        testEndangered.save();
-//        testEndangered.delete();
-//        assertEquals(null, Endangered.find(testEndangered.getId()));
-//    }
-//
-//
-//
-//}
+package modules;
+import org.junit.*;
+
+import java.sql.Timestamp;
+import java.util.Date;
+
+import static org.junit.Assert.*;
+
+public class EndangeredTest {
+
+    public Endangered setUpNewEndangered() {
+        return new Endangered("rhino","ill","young");
+    }
+
+    @Rule
+    public DatabaseRule database = new DatabaseRule();
+
+    @Test
+    public void Endangered_instantiatesCorrectly_true() {
+        Endangered testEndangered = setUpNewEndangered();
+        assertEquals(true,testEndangered instanceof Endangered);
+    }
+
+    @Test
+    public void getName_instantiatesWithName_String() {
+        Endangered testEndangered = setUpNewEndangered();
+        assertEquals("rhino", testEndangered.getName());
+    }
+
+
+    @Test
+    public void getHealth_instantiatesWithHealth_String() {
+        Endangered testEndangered = setUpNewEndangered();
+        assertEquals("ill", testEndangered.getHealth());
+    }
+
+    @Test
+    public void getAge_instantiatesWithAge_String() {
+        Endangered testEndangered = setUpNewEndangered();
+        assertEquals("young", testEndangered.getAge());
+    }
+
+
+
+    @Test
+    public void equals_returnsTrueIfObjectsSame_true() {
+        Endangered testEndangered = setUpNewEndangered();
+        Endangered otherEndangered = setUpNewEndangered();
+        assertTrue(testEndangered.equals(otherEndangered));
+    }
+
+    @Test
+    public void save_savesObjectsToDatabase_true() {
+        Endangered testEndangered = setUpNewEndangered();
+        testEndangered.save();
+        assertTrue(Endangered.all().get(0).equals(testEndangered));
+    }
+
+    @Test
+    public void save_assignsIdToEndangered() {
+        Endangered testEndangered = setUpNewEndangered();
+        testEndangered.save();
+        Endangered savedEndangered = Endangered.all().get(0);
+        assertEquals(savedEndangered.getId(), testEndangered.getId());
+    }
+
+    @Test
+    public void all_returnsAllInstancesOfEndangered_true() {
+        Endangered testEndangered = setUpNewEndangered();
+        testEndangered.save();
+        Endangered otherEndangered = new Endangered("","","");
+        otherEndangered.save();
+        assertEquals(true,Endangered.all().get(0).equals(testEndangered));
+        assertEquals(true,Endangered.all().get(1).equals(otherEndangered));
+    }
+
+    @Test
+    public void find_returnsEndangeredWithSameId_otherEndangered() {
+        Endangered firstEndangered =setUpNewEndangered();
+        firstEndangered.save();
+        Endangered secondEndangered = new Endangered("","","");
+        secondEndangered.save();
+        assertEquals(Endangered.find(secondEndangered.getId()), secondEndangered);
+    }
+
+    @Test
+    public void delete_deletesEndangeredAnimal_true() {
+        Endangered testEndangered = setUpNewEndangered();
+        testEndangered.save();
+        testEndangered.delete();
+        assertEquals(null, Endangered.find(testEndangered.getId()));
+    }
+
+
+
+}
--- src/test/java/modules/SightingTest.java
+++ src/test/java/modules/SightingTest.java
@@ -1,87 +1,87 @@
-//package modules;
-//
-//import org.sql2o.*;
-//import org.junit.*;
-//import static org.junit.Assert.*;
-//import java.util.Arrays;
-//
-//import static org.junit.Assert.*;
-//
-//
-//public class SightingTest {
-//
-//    @Rule
-//    public DatabaseRule database = new DatabaseRule();
-//
-//    public Sighting setUpNewSighting(){
-//        return new Sighting("ZoneA","Kevin",1 );
-//    }
-//    @Test
-//    public void sighting_instantiatesCorrectly_true() {
-//        Sighting testSighting = new Sighting("ZoneA","Kevin", 1);
-//    }
-//
-//    @Test
-//    public void getLocation_sightingInstantiatesWithLocation_ZoneA() {
-//        Sighting testSighting = new Sighting("ZoneA","Kevin", 1);
-//        assertEquals("ZoneA", testSighting.getLocation());
-//
-//    }
-//
-//    @Test
-//    public void getRanger_sightingInstantiatesWithRanger_Kevin() {
-//        Sighting testSighting = new Sighting("ZoneA","Kevin",1);
-//        assertEquals("Kevin", testSighting.getRanger());
-//    }
-//
-//    @Test
-//    public void equals_returnsTrueIfLocationAndRangerAreSame_true() {
-//        Sighting testSighting = setUpNewSighting();
-//        Sighting otherSighting = setUpNewSighting();
-//        assertTrue(testSighting.equals(otherSighting));
-//    }
-//
-//    @Test
-//    public void save_insertsObjectIntoDatabase_Sighting() {
-//        Sighting testSighting = setUpNewSighting();
-//        testSighting.save();
-//        assertEquals(Sighting.all().get(0), testSighting);
-//    }
-//
-//    @Test
-//    public void all_returnsAllInstancesOfSighting_true() {
-//        Sighting testSighting = setUpNewSighting();
-//        testSighting.save();
-//        Sighting otherSighting = new Sighting("","",1);
-//        otherSighting.save();
-//        assertEquals(true,Sighting.all().get(0).equals(testSighting));
-//        assertEquals(true,Sighting.all().get(1).equals(otherSighting));
-//    }
-//
-//    @Test
-//    public void save_assignsIdToObject() {
-//        Sighting testSighting = setUpNewSighting();
-//        testSighting.save();
-//        Sighting savedSighting = Sighting.all().get(0);
-//        assertEquals(testSighting.getId(),savedSighting.getId());
-//    }
-//
-//    @Test
-//    public void find_returnsSightingWithSameId_otherSighting() {
-//        Sighting testSighting = setUpNewSighting();
-//        testSighting.save();
-//        Sighting otherSighting = new Sighting("","",1);
-//        otherSighting.save();
-//        assertEquals(Sighting.find(otherSighting.getId()), otherSighting);
-//    }
-//
-//
-//
-//    @Test
-//    public void delete_deletesSighting_true() {
-//        Sighting newSighting = setUpNewSighting();
-//        newSighting.save();
-//        newSighting.delete();
-//        assertEquals(0, Sighting.all().size());
-//    }
-//}
+package modules;
+
+import org.sql2o.*;
+import org.junit.*;
+import static org.junit.Assert.*;
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
+
+public class SightingTest {
+
+    @Rule
+    public DatabaseRule database = new DatabaseRule();
+
+    public Sighting setUpNewSighting(){
+        return new Sighting("ZoneA","carlos",1 );
+    }
+    @Test
+    public void sighting_instantiatesCorrectly_true() {
+        Sighting testSighting = new Sighting("ZoneA","carlos", 1);
+    }
+
+    @Test
+    public void getLocation_sightingInstantiatesWithLocation_ZoneA() {
+        Sighting testSighting = new Sighting("ZoneA","carlos", 1);
+        assertEquals("ZoneA", testSighting.getLocation());
+
+    }
+
+    @Test
+    public void getRanger_sightingInstantiatesWithRanger_Kevin() {
+        Sighting testSighting = new Sighting("ZoneA","carlos",1);
+        assertEquals("carlos", testSighting.getRanger());
+    }
+
+    @Test
+    public void equals_returnsTrueIfLocationAndRangerAreSame_true() {
+        Sighting testSighting = setUpNewSighting();
+        Sighting otherSighting = setUpNewSighting();
+        assertTrue(testSighting.equals(otherSighting));
+    }
+
+    @Test
+    public void save_insertsObjectIntoDatabase_Sighting() {
+        Sighting testSighting = setUpNewSighting();
+        testSighting.save();
+        assertEquals(Sighting.all().get(0), testSighting);
+    }
+
+    @Test
+    public void all_returnsAllInstancesOfSighting_true() {
+        Sighting testSighting = setUpNewSighting();
+        testSighting.save();
+        Sighting otherSighting = new Sighting("","",1);
+        otherSighting.save();
+        assertEquals(true,Sighting.all().get(0).equals(testSighting));
+        assertEquals(true,Sighting.all().get(1).equals(otherSighting));
+    }
+
+    @Test
+    public void save_assignsIdToObject() {
+        Sighting testSighting = setUpNewSighting();
+        testSighting.save();
+        Sighting savedSighting = Sighting.all().get(0);
+        assertEquals(testSighting.getId(),savedSighting.getId());
+    }
+
+    @Test
+    public void find_returnsSightingWithSameId_otherSighting() {
+        Sighting testSighting = setUpNewSighting();
+        testSighting.save();
+        Sighting otherSighting = new Sighting("","",1);
+        otherSighting.save();
+        assertEquals(Sighting.find(otherSighting.getId()), otherSighting);
+    }
+
+
+
+    @Test
+    public void delete_deletesSighting_true() {
+        Sighting newSighting = setUpNewSighting();
+        newSighting.save();
+        newSighting.delete();
+        assertEquals(0, Sighting.all().size());
+    }
+}
